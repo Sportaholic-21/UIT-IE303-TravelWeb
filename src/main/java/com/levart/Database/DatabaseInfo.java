09e3a05040deff8e4a4cc7b3ecb7b14740b068dd
Long Nguyễn Phúc <18521044@gm.uit.edu.vn>
2021-04-17 19:20:08 +0700

    #13 Create db config

---
+++ src/main/java/com/levart/Database/DatabaseInfo.java
@@ -0,0 +1,7 @@
+package com.levart.Database;
+
+public class DatabaseInfo {
+    public static String url = "jdbc:sqlserver://localhost:1433;databaseName=Levart;";
+    public static String user = "sa";
+    public static String pass = "1234";
+}
---
+++ src/main/java/com/levart/Database/DatabaseManager.java
@@ -0,0 +1,17 @@
+package com.levart.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    protected Connection conn;
+    //Kết nối Database
+    public Connection Connect() throws SQLException 
+    {
+        conn = DriverManager.getConnection(DatabaseInfo.url, DatabaseInfo.user, DatabaseInfo.pass);
+        if (conn != null)
+            System.out.println("Connected");
+        return conn;
+    }
+}
