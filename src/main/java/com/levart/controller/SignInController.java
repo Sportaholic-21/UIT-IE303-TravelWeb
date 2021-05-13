2d8b5b581d0fd687e6e62ad5ba780cd5462e6555
Long Nguyễn Phúc <18521044@gm.uit.edu.vn>
2021-05-13 18:50:59 +0700

    #13 config sign up post

--- src/main/java/com/levart/controller/SignInController.java
+++ src/main/java/com/levart/controller/SignInController.java
@@ -1,13 +1,19 @@
 package com.levart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.levart.entities.User;
 import com.levart.form_entities.FormSearch;
 import com.levart.form_entities.FormSignUp;
+import com.levart.hibernate.dao.UserDAO;
 
 @Controller
+@SessionAttributes("contentSignIn")
 public class SignInController {
 	@ModelAttribute("contentSignIn")
 	public FormSignUp setSignIn() {
@@ -22,7 +28,32 @@ public class SignInController {
 		return new FormSearch();
 	}
 	@RequestMapping("/sign-in")
-	public String showPage() {
+	public String showPage(Model model) {
+		FormSignUp userForm = new FormSignUp();
+		model.addAttribute("contentSignUp", userForm);
 		return "sign-in";
+	}
+	@PostMapping("/signIn")
+	public String handleSignIn(@ModelAttribute("contentSignIn") FormSignUp userForm){
+		
+		return "redirect: home"; 
+	}
+	@PostMapping("/signUp")
+	public String handleSignUp(@ModelAttribute("contentSignUp") FormSignUp userForm){
+		UserDAO userDAO = new UserDAO();
+		User user = new User();
+		user.setUsername(userForm.getName());
+		user.setEmail(userForm.getEmail());
+		user.setPass(userForm.getPass());
+		user.setPhone(userForm.getPhone());
+		user.setUserRole(0);
+		try{
+			userDAO.addUser(user);
+			userDAO.getUser();
+			System.out.println(user.getPhone());
+		} catch(Exception e){
+			return "redirect: sign-in";
+		}
+		return "redirect: home";
 	}
 }
