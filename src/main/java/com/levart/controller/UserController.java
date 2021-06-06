79463cd68659ef6e938040e1a3dc9378a898ccf0
Lan <truongylan2000@gmail.com>
2021-06-06 11:20:10 +0700

    #27 Fill User Overview Page with data

--- src/main/java/com/levart/controller/UserController.java
+++ src/main/java/com/levart/controller/UserController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.levart.entities.Account;
+import com.levart.entities.Nation;
 import com.levart.entities.TourBooking;
 import com.levart.hibernate.dao.AccountDAO;
+import com.levart.hibernate.dao.NationDAO;
 import com.levart.hibernate.utils.CRUDBookedTourOperation;
 
 @Controller
@@ -21,12 +23,59 @@ public class UserController extends CRUDBookedTourOperation {
 	@RequestMapping(value={"", "/"})
 	public String showPage(@PathVariable String username, org.springframework.web.context.request.WebRequest webRequest, Model model) {
 		AccountDAO accountDAO = new AccountDAO();
+		NationDAO nationDAO = new NationDAO();
+		
 		String tab = webRequest.getParameter("tab");
 		
 		if (tab == null) {
-			Account account = accountDAO.getAccount(username);
+			Account account = accountDAO.getAccountWithTourBooking(username);
+			int totalFeedbacks = 0;
+			int count = 0;
+			
+			List<TourBooking> endedTours = new ArrayList<TourBooking>();
+			
+			List<TourBooking> tourBookings = account.getTourBookings();
+			List<String> coordinates = new ArrayList<String>();
+			List<String> tourNames = new ArrayList<String>();
+			List<Integer> isFeedbacks = new ArrayList<Integer>();
+			List<Float> stars = new ArrayList<Float>();
+			List<String> dates = new ArrayList<String>();
+			
+			List<Nation> nations = nationDAO.getListNationByEndedTour();
+			
+			Iterator<TourBooking> tourBookingItem = tourBookings
+					.iterator();
+			while (tourBookingItem.hasNext()) {
+				TourBooking tourBKItem = tourBookingItem.next();
+				if (tourBKItem.getBookStatus() == 3) {
+					endedTours.add(tourBKItem);
+					
+					String coordinate = tourBKItem.getTour().getCoordinate();
+					coordinates.add(coordinate);
+					
+					tourNames.add(tourBKItem.getTour().getTourName());
+					dates.add(tourBKItem.getScheduleDate());
+					
+					if(tourBKItem.getFeedback() != null) {
+						totalFeedbacks++;
+						
+						stars.add(count, tourBKItem.getFeedback().getStart());
+					} else {
+						isFeedbacks.add(0);
+					}
+					
+					count++;
+				}
+			}
 			
 			model.addAttribute("account", account);
+			model.addAttribute("totalFeedbacks", totalFeedbacks);
+			model.addAttribute("coordinates", coordinates);
+			model.addAttribute("tourNames", tourNames);
+			model.addAttribute("totalNations", nations.size());
+			model.addAttribute("dates", dates);
+			model.addAttribute("stars", stars);
+			model.addAttribute("endedTours", endedTours);
 			
 			return "/user/guest-profile";
 		}
@@ -52,7 +101,6 @@ public class UserController extends CRUDBookedTourOperation {
 			List<TourBooking> tourCancels = new ArrayList<TourBooking>();
 			
 			List<TourBooking> tourBookings = account.getTourBookings();
-			System.out.println(tourBookings);
 			Iterator<TourBooking> tourBookingItem = tourBookings
 					.iterator();
 			while (tourBookingItem.hasNext()) {
